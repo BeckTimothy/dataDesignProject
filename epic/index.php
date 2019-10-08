e4e688218518625b04e280473599c934f7ff0f68
Timothy Beck <johndaveleet@gmail.com>
2019-10-08 09:44:22 -0600

    created index.php, phase-1.php, and style.css

---
+++ epic/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Data Design Project</title>
+	<link rel="stylesheet" type="text/css" href="./styles.css">
+</head>
+<body>
+	<h1>Data Design: Reddit</h1>
+	<a href="./phase-1.php">Persona, User Story, Use Case/Interaction Flow</a>
+</body>
+</html>
---
+++ epic/phase-1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<HTML lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Data Design</title>
+	<link rel="stylesheet" type="text/css" href="./styles.css">
+</head>
+<body>
+	<div>
+		<h2>Persona:</h2>
+		<p>BobMcHateseverything, a 32 year old male living in his mother’s basement, with above average computing skills. He’s extremely opinionated about everything, claims to love technology but only uses Windows and despises Apple products. He uses Reddit to flex his opinions, argue with everyone, and correct their typos.</p>
+		<h2>User Story:</h2>
+		<p>“As a reddit user, I want to downvote every post that disagrees with me.”</p>
+		<h2>Use Case/Interaction</h2>
+		<p>u/BobMcHatesEverything has been arguing in the comment section of a popular computer related subreddit, however everyone is vehemently disagreeing with his opinions.u/BobMcHatesEverything feels the need to downvote every single post that doesn’t share his perspective to show the other users just how wrong they are.
+
+			<em>Precondition:</em> u/BobMcHatesEverything received a reply he doesn’t like.
+
+			<em>Postcondition:</em> The other users feel bad about being incorrect.
+		</p>
+		<ul>
+			<li>Bob receives a reply to one of his comments, in his inbox, disagreeing with his opinion about Mac computers.</li>
+			<li>He immediately clicks the downvote icon, to put the opposing user in his rightful place.</li>
+			<li>The website changes the karma of the other user’s reply from 1 to 0</li>
+			<li>Bob then clicks reply, prompting him with an input field to type a reply.</li>
+			<li>Bob crafts a colorful message about how his opinion is correct and everyone else is wrong.</li>
+			<li>Clicking the reply button again, his comment reply is displayed below the other user’s comment.</li>
+			<li>The opposing user has been dealt with accordingly.</li>
+		</ul>
+	</div>
+</body>
+
+<?php
+?>
