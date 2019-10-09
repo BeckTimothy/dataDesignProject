de1087ec1bf8664f76e8faac4c98e96513807dbf
Timothy Beck <johndaveleet@gmail.com>
2019-10-09 13:07:52 -0600

    changed link text in index.php to be more accurate.

--- epic/index.php
+++ epic/index.php
@@ -8,7 +8,7 @@
 <body class="center">
 	<div>
 		<h1>Data Design: Reddit</h1>
-		<a href="./phase-1.php">Persona, User Story, Use Case/Interaction Flow</a>
+		<a href="./phase-1.php">Persona</a>
 		<a href="./user-story.php">User Story</a>
 		<a href="./use-case-interaction.php">Use Case/Interaction Flow</a>
 	</div>
